8080d2bed92e647aac42386d98e15ed15624772a
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-09-26 18:07:11 +0530

    Biometric Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -35,14 +35,43 @@ import "package:flutter_basics/for_loop.dart";
 import "package:flutter_basics/listmodal/listview.dart";
 import "package:local_auth/local_auth.dart";
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final LocalAuthentication auth;
+
+  @override
+  void initState() {
+    super.initState();
+    auth = LocalAuthentication();
+    _authenticate();
+  }
+
+  Future<void> _authenticate() async {
+    try {
+      bool authenticated = await auth.authenticate(
+        localizedReason: 'Please Authenticate Yourself',
+        options: AuthenticationOptions(
+          stickyAuth: true,
+          biometricOnly: false, //* Allows only biometrics no password/pin
+        ),
+      );
+      print('Authenticated : $authenticated');
+    } on PlatformException catch (c) {
+      print(c);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,86 +82,7 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color.fromARGB(255, 0, 0, 255),
         brightness: Brightness.dark,
       ),
-      home: const BiometricAuth(),
+      home: const GlowingAvatar(),
     );
   }
 }
-
-class BiometricAuth extends StatefulWidget {
-  const BiometricAuth({super.key});
-
-  @override
-  State<BiometricAuth> createState() => _BiometricAuthState();
-}
-
-class _BiometricAuthState extends State<BiometricAuth> {
-  late final LocalAuthentication auth;
-  bool _supportState = false;
-
-  //^ Checks the device supports the biometric authentication
-  @override
-  void initState() {
-    super.initState();
-    auth = LocalAuthentication();
-    auth.isDeviceSupported().then(
-          (bool isDeviceSupported) => setState(() {
-            _supportState = isDeviceSupported;
-          }),
-        );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Biometric Authentication'),
-        backgroundColor: Color.fromARGB(255, 0, 0, 255),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(_supportState
-              ? 'This Device is Supported'
-              : 'This Device is Not Supported'),
-          Divider(),
-          ElevatedButton(
-            onPressed: _getAvailableBiometrics,
-            child: Text('Get Availabe Biometrics'),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(onPressed: _authenticate, child: Text('Authenticate')),
-        ],
-      ),
-    );
-  }
-
-  Future<void> _authenticate() async {
-    try {
-      bool authenticated = await auth.authenticate(
-        localizedReason: 'Please Authenticate Yourself',
-        options: AuthenticationOptions(
-            stickyAuth: true,
-            biometricOnly: false //* Allows only biometrics no password/pin
-            ),
-      );
-      print('Authenticated : $authenticated');
-      if (authenticated) {
-        Navigator.of(context).push(MaterialPageRoute(builder: (_) => GlowingAvatar()));
-      }
-    } on PlatformException catch (c) {
-      print(c);
-    }
-  }
-
-  Future<void> _getAvailableBiometrics() async {
-    List<BiometricType> availableBiometrics =
-        await auth.getAvailableBiometrics();
-
-    print("List of Biometrics = $availableBiometrics");
-
-    if (!mounted) {
-      return;
-    }
-  }
-}
-
